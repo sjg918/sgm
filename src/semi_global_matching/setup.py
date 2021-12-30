0ea0c4169056a242ee6e75756f15962dc9192bef
jingyuSong <68323609+sjg918@users.noreply.github.com>
2021-12-30 21:20:17 +0900

    Update setup.py

--- src/semi_global_matching/setup.py
+++ src/semi_global_matching/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='sigmoid_focal_loss',
+    name='semi_global_matching',
     ext_modules=[
-        CUDAExtension('sigmoid_focal_loss_cuda', [
-            'src/sigmoid_focal_loss.cpp',
-            'src/sigmoid_focal_loss_cuda.cu',
+        CUDAExtension('semi_global_matching_cuda', [
+            'src/semi_global_matching.cpp',
+            'src/semi_global_matching_cuda.cu',
         ],
         extra_compile_args={'cxx': ['-g'],
                             'nvcc': ['-O2']})
