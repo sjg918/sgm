d5cac2cf6ae49b11342624b63202cd4b6dc65ebc
jingyuSong <68323609+sjg918@users.noreply.github.com>
2021-12-30 21:38:11 +0900

    Update utils.py

--- src/semi_global_matching/utils.py
+++ src/semi_global_matching/utils.py
@@ -5,16 +5,16 @@ import semi_global_matching_cuda
 def sgm_gpu(left, right):
     assert len(left.shape) == 2
     assert len(right.shape) == 2
-    assert (left.shape[0] // 4 == 0) and (left.shape[1] // 4 == 0)
-    assert (right.shape[0] // 4 == 0) and (right.shape[1] // 4 == 0)
+    assert (left.shape[0] % 4 == 0) and (left.shape[1] % 4 == 0)
+    assert (right.shape[0] % 4 == 0) and (right.shape[1] % 4 == 0)
 
     H, W = left.shape
 
-    left = left.to(torch.uint8).cuda()
-    right = right.to(torch.uint8).cuda()
+    left = left.cuda()
+    right = right.cuda()
 
     dispmap = semi_global_matching_cuda.sgm(
-        left, right, 7, 86, H, W)
+        left, right, 7, 92, H, W)
 
     return dispmap
 
