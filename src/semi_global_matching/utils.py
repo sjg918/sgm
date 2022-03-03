fd928bd6a7f2d2cb2f0f85d4714eb24f36611c86
jingyuSong <68323609+sjg918@users.noreply.github.com>
2022-03-03 19:30:51 +0900

    Update utils.py

--- src/semi_global_matching/utils.py
+++ src/semi_global_matching/utils.py
@@ -2,7 +2,7 @@
 import torch
 import semi_global_matching_cuda
 
-def sgm_gpu(left, right):
+def mkDispmap(left, right, pathA, return_gpu=True):
     assert len(left.shape) == 2
     assert len(right.shape) == 2
     assert (left.shape[0] % 4 == 0) and (left.shape[1] % 4 == 0)
@@ -13,10 +13,18 @@ def sgm_gpu(left, right):
     left = left.cuda()
     right = right.cuda()
 
-    dispmap = semi_global_matching_cuda.sgm(
-        left, right, 7, 92, H, W)
 
-    return dispmap
+    if pathA == 2:
+        dispmap = semi_global_matching_cuda.sgm(left, right, 7, 84, H, W, 2)
+    elif pathA == 4:
+        dispmap = semi_global_matching_cuda.sgm(left, right, 7, 92, H, W, 4)
+    elif pathA == 8:
+        dispmap = semi_global_matching_cuda.sgm(left, right, 6, 96, H, W, 8)
+
+    if return_gpu:
+        return dispmap
+    else:
+        return dispmap.cpu()
 
 if __name__ == '__main__':
     print('hi')
